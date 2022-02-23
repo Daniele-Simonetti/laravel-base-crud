82d0fb12994b5c7bff0beed113d38362ed698054
Daniele-Simonetti <daniele.simonetti99@outlook.it>
2022-02-23 17:58:55 +0100

    adding structure

--- database/migrations/2022_02_23_131101_create_comics_table.php
+++ database/migrations/2022_02_23_131101_create_comics_table.php
@@ -15,6 +15,13 @@ class CreateComicsTable extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 100);
+            $table->text('description');
+            $table->string('thumb');
+            $table->float('price');
+            $table->string('series', 150);
+            $table->string('type', 150);
+            $table->date('sale_date');
             $table->timestamps();
         });
     }
--- database/seeds/ComicSeeder.php
+++ database/seeds/ComicSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use Faker\Generator as Faker;
+use App\Comic;
 use Illuminate\Database\Seeder;
 
 class ComicSeeder extends Seeder
@@ -9,8 +11,17 @@ class ComicSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i=0; $i < 100; $i++) { 
+            $newComic = new Comic();
+            $newComic->title = $faker->words(2, true);
+            $newComic->description = $faker->paragraph(3, true);
+            $newComic->thumb = $faker->imageUrl(640, 480, 'comics', true);
+            $newComic->price = $faker->randomFloat(2, 15, 25);
+            $newComic->series = $faker->words(3, true);
+            $newComic->type = $faker->words(1, true);
+            $newComic->sale_date = $faker->date();
+        }
     }
 }
